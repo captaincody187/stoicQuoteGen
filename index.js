c7f3601370c5de53aeb589dafef69aa9b6ff5ca3
cody simon <86523258+captaincody187@users.noreply.github.com>
2022-11-22 18:51:38 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+const imageArray = [
+"marcus.jpg",
+"alexis.jpg",
+"aristotle.jpg",
+"epictetus.jpg"
+];
+
+
+
+
+
+const image = document.querySelector("img");
+const button = document.querySelector("button");
+
+window.onload = () => generateRandomPicture(imageArray);
+
+button.addEventListener("click", () => generateRandomPicture(imageArray));
+
+function generateRandomPicture(array){
+	let randomNum = Math.floor(Math.random() * array.length); 
+	image.setAttribute("src", array[randomNum]);
+}
